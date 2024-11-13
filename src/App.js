45c8c5d31ba2332b52ec5487ac249f8f280eac74
imbharath95 <147942751+imbharath95@users.noreply.github.com>
2024-11-13 15:27:21 -0500

    item details

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.scss";
+
+
+
+export default function () {
+  const [sampleItemDetails, setSampleItemDetails] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+    .then(res => res.json())
+    .then(data => setSampleItemDetails(data))
+  })
+  return (
+   <div className="item-box-container">
+    <ul>
+    {sampleItemDetails.map(item => {
+      return <li>{item.title}</li>
+    })}
+    </ul>
+      
+           
+      </div>
+  );
+}
